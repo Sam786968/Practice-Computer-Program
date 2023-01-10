d43615d29347df1b4eb07722d0564917060d1cfb
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2023-01-10 20:47:21 +0000

    the jokes are had

--- program.py
+++ program.py
@@ -1,37 +1,37 @@
 def he_has_jokes():
   print("Welcome to the knock knock joke library")
   joke = input("Do you want to hear a joke? (Yes or No) \n")
-  
-
-  punchlines = ["Calder police- I've been robbed! ", "You are welcome! " ,  ]
+  question = input("Do you want to hear a joke about robbers, tanks , or pencils? " )
+  punchlines = ["Calder police- I've been robbed! ", "You are welcome! " , "Nevermind, it's pointless! "  ]
   if joke == "no":
    print("Okay suit yourself")
   while (joke == "yes"):
-   print("Great, Lets Play")
-   question = input("Do you want to hear a joke about robbers, tanks , or pencils? " )
-   if question == "robbers":
-     input("Knock Knock ")
-     input("Calder")
-     print("Calder police- I've been robbed!")
-     joke = input("Do you want to hear another joke or are you finished? ")
-   elif question == "tanks":
-     input("Knock Knock ")
-     input("Tank ")
-     input("You are welcome! ")
-     joke = input("Do you want to hear another joke or are you finished? ")
-   elif question == "pencils":
-     input("Knock Knock ")
-     input("Broken pencil ")
-     input("Nevermind, it's pointless! ")
-     joke = input("Do you want to hear another joke or are you finished? ")
-   if joke == "finished" :
+    print("Great, Lets Play")
+    he_actually_has_the_jokes(question, punchlines)
+    joke = input("Do you want to hear another joke or are you finished? ")
+  if joke == "finished":
      rate = int(input("Please rate our game 1-10! "))
      final_score = int((rate * 10))
-     print(str(final_score)  +  "percent satisfaction rate")
+     print(str(final_score)  +  " percent satisfaction rate")
      friend = input("Would you recommend this game to a friend? ")
-    
      print (friend)
      if friend == "yes" or friend == "maybe" :
        print("Thanks, we appreciate it. ")
      else:
        print("Sorry you did not enjoy it. ")
+  else:
+    he_actually_has_the_jokes()
+
+def he_actually_has_the_jokes(question,punchlines):
+  punchlines = ["Calder police- I've been robbed! ", "You are welcome! " , "Nevermind, it's pointless! "]
+  input("Knock Knock ")
+  if question == "robbers":
+     input("Calder")
+     print(punchlines[0])
+  elif question == "tanks":
+     input("Tank ")
+     input(punchlines[1])
+  elif question == "pencils":
+     input("Broken pencil ")
+     input(punchlines[2])
+  
